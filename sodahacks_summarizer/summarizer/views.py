397c43fbfcd9a0964fb49916aac4bc491a58b944
Shaurya Sanghvi <shaurya_s@berkeley.edu>
2018-04-15 00:08:20 -0700

    worked on some shit

--- sodahacks_summarizer/summarizer/views.py
+++ sodahacks_summarizer/summarizer/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django import forms
 
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the summarizer index.")
+	return render(request, 'summarizer/index.html')
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
