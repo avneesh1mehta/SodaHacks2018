6e0e1c70d99837050b971c499bff072fa93c8369
Shaurya Sanghvi <shaurya_s@berkeley.edu>
2018-04-15 09:32:45 -0700

    deployment

--- sodahacks_summarizer/summarizer/templates/summarizer/forms.py
+++ sodahacks_summarizer/summarizer/templates/summarizer/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 class TextForm(forms.Form):
-    num_sentences = forms.IntegerField(label="Number of Output Sentences", widget=forms.NumberInput(attrs={'class': 'text-muted form-control bg-dark', 'style': 'width: 12ch','placeholder': 0}))
+    num_sentences = forms.IntegerField(label="Number of Output Sentences", widget=forms.NumberInput(attrs={'class': 'text-muted form-control bg-dark', 'style': 'width: 18ch','placeholder': 0}))
     content = forms.CharField(label="Text to Summarize", widget=forms.Textarea(attrs={'class': 'text-muted form-control bg-dark', 'placeholder':'Lorem Ipsum Dolor...'}))
