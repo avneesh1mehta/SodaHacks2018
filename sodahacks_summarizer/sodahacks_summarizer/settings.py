dd10b9d402c2a0f790cb75d16f3d0b0263885ee7
avneesh1mehta <avneesh1mehta@hotmail.com>
2018-04-14 23:04:18 -0700

    Added routes

--- sodahacks_summarizer/sodahacks_summarizer/settings.py
+++ sodahacks_summarizer/sodahacks_summarizer/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'summarizer.apps.SummarizerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
